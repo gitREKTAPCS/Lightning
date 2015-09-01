43c4821e62688e2894a2f6b3ef984b7dbff825e5
unknown <jelin1@697-334-Erech.sf.k12.internal>
2015-09-01 10:25:52 -0700

    Final stuff

--- Lightning.pde
+++ Lightning.pde
@@ -2,6 +2,9 @@ int startX=(int)(Math.random() * 250);
 int startY=0;
 int endX=0;
 int endY=150;
+int redColor;
+int greenColor;
+int blueColor;
 
 void setup()
 {
@@ -12,10 +15,11 @@ void setup()
 }
 void draw()
 {
-  int redColor = (int)(Math.random()*256);
-  int greenColor = (int)(Math.random()*256);
-  int blueColor = (int)(Math.random()*256);
+	
   while (endY < 300) {
+  	redColor = (int)(Math.random()*256);
+	greenColor = (int)(Math.random()*256);
+	blueColor = (int)(Math.random()*256);
     stroke(redColor, greenColor, blueColor);
     endX = (startX + (int)(Math.random() *20)-10);
     endY = (startY + (int)(Math.random() *10));
@@ -23,6 +27,7 @@ void draw()
     startX=endX;
     startY=endY;
   }
+  clouds();
 }
 
 void mousePressed()
@@ -33,4 +38,15 @@ void mousePressed()
   endX=0;
   endY=150;
   redraw();
+}
+
+void clouds(){
+	noStroke();
+	fill(redColor,greenColor, blueColor);
+	ellipse(50, 30, 120, 120);
+	ellipse(100, 30, 120, 120);
+	ellipse(150, 30, 120, 120);
+	ellipse(200, 30, 120, 120);
+	ellipse(250, 30, 120, 120);
+	
 }
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,10 +14,13 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX=0;
-int startY=150;
-int endX=150;
-int endY=0;
+int startX=(int)(Math.random() * 250);
+int startY=0;
+int endX=0;
+int endY=150;
+int redColor;
+int greenColor;
+int blueColor;
 
 public void setup()
 {
@@ -28,21 +31,40 @@ public void setup()
 }
 public void draw()
 {
-	int redColor = (int)(Math.random()*256);
-	int greenColor = (int)(Math.random()*256);
-	int blueColor = (int)(Math.random()*256);
-	while (endX < 300) {
-		stroke(redColor, greenColor, blueColor);
-		endX = (startX + (int)(Math.random() *10));
-		endY = (startY + (int)(Math.random() *20)-10);
-		line(startX, startY, endX, endY);
-		startX=endX;
-		startY=endY;
-	}
+	
+  while (endY < 300) {
+  	redColor = (int)(Math.random()*256);
+	greenColor = (int)(Math.random()*256);
+	blueColor = (int)(Math.random()*256);
+    stroke(redColor, greenColor, blueColor);
+    endX = (startX + (int)(Math.random() *20)-10);
+    endY = (startY + (int)(Math.random() *10));
+    line(startX, startY, endX, endY);
+    startX=endX;
+    startY=endY;
+  }
+  clouds();
 }
+
 public void mousePressed()
 {
-	redraw();
+  System.out.println("works");
+  startX=(int)(Math.random() * 300);
+  startY=0;
+  endX=0;
+  endY=150;
+  redraw();
+}
+
+public void clouds(){
+	noStroke();
+	fill(redColor,greenColor, blueColor);
+	ellipse(50, 30, 120, 120);
+	ellipse(100, 30, 120, 120);
+	ellipse(150, 30, 120, 120);
+	ellipse(200, 30, 120, 120);
+	ellipse(250, 30, 120, 120);
+	
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
